366a0feccb171897f142fd4cd1387239c55d736c
Sami <sami.zara91@gmail.com>
2017-07-18 12:28:15 +0300

    Logo addedd,enemy reborn bug fixed

--- Assets/Scripts/Game/Enemy.cs
+++ Assets/Scripts/Game/Enemy.cs
@@ -105,7 +105,35 @@ public class Enemy : MonoBehaviour
 
     public void Reborn()
     {
-        Vector2 pos = Vector3.zero;
+        float minX = float.MaxValue;
+        float minY = float.MaxValue;
+        float maxX = float.MinValue;
+        float maxY = float.MinValue;
+        foreach (Vector2 point in GameReferenceManager.instance.map.allPoints)
+        {
+            if (point.x < minX)
+                minX = point.x;
+            if (point.x > maxX)
+                maxX = point.x;
+            if (point.y < minY)
+                minY = point.y;
+            if (point.y > maxY)
+                maxY = point.y;
+        }
+        minX += 0.6f;
+        minY += 0.6f;
+        maxX -= 0.6f;
+        maxY -= 0.6f;
+        Vector2 pos = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+        while(!MathHelper.ContainsPoint(GameReferenceManager.instance.map.allPoints.ToArray(), pos))
+        {
+            pos = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        }
+        transform.position = GameReferenceManager.instance.map.MapToWorldPoint(pos);
+        isDead = false;
+        sprite.enabled = true;
+        collider.enabled = true;
+        /*Vector2 pos = Vector3.zero;
         int counter = 0;
         foreach(Vector2 point in GameReferenceManager.instance.map.allPoints)
         {
@@ -115,7 +143,7 @@ public class Enemy : MonoBehaviour
         transform.position = pos / counter;
         isDead = false;
         sprite.enabled = true;
-        collider.enabled = true;
+        collider.enabled = true;*/
     }
 
 }
